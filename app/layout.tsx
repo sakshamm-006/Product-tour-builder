6fa162c78d39834494374ad53474f61f3a11f9e7
Saksham Yadav <144156171+sakshamm-006@users.noreply.github.com>
2026-01-20 16:28:02 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
+// exporting const metadata
 export const metadata: Metadata = {
   title: 'v0 App',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
-
+//exporting default function 
 export default function RootLayout({
   children,
 }: Readonly<{
